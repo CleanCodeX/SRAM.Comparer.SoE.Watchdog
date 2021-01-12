3d29ee0bfa91a5a3d0b2b051e8ecfff46f22a8b5
CleanCodeX <69156989+SoE-Coding@users.noreply.github.com>
2021-01-12 20:26:00 +0100

    added exception handling embed debug symbols for release

--- Program.cs
+++ Program.cs
@@ -13,45 +13,53 @@ namespace SramComparer.SoE.FileWatcher
 
 		private static void Main(string[] args)
 		{
-			var options = new CmdLineParserSoE().Parse(args);
-
-			ConsoleHelper.Initialize(options);
-			ConsoleHelper.PrintParams(args, options);
-			ConsoleHelper.PrintOptions(WatchOptions);
-			ConsoleHelper.PrintHelp();
-
-			using var fileSystemWatcher = FileWatcherHelper.StartWatching(options, WatchOptions);
-
-			while (true)
+			try
 			{
-				var key = Console.ReadLine()!.ToLower();
+				var options = new CmdLineParserSoE().Parse(args);
 
-				try
+				ConsoleHelper.Initialize(options);
+				ConsoleHelper.PrintParams(args, options);
+				ConsoleHelper.PrintOptions(WatchOptions);
+				ConsoleHelper.PrintHelp();
+
+				using var fileSystemWatcher = FileWatcherHelper.StartWatching(options, WatchOptions);
+
+				while (true)
 				{
-					switch (key)
-					{
-						case "??":
-							ConsoleHelper.PrintHelp();
-							continue;
-						case "auto_e":
-							WatchOptions.AutoExport = !WatchOptions.AutoExport;
-							ConsoleHelper.PrintOption(nameof(WatchOptions.AutoExport), WatchOptions.AutoExport);
-							continue;
-						case "auto_o":
-							WatchOptions.AutoOverwrite = !WatchOptions.AutoOverwrite;
-							ConsoleHelper.PrintOption(nameof(WatchOptions.AutoOverwrite), WatchOptions.AutoOverwrite);
-							continue;
-						default:
-							if (!CommandHelper.RunCommand(key, options))
-								return;
+					var key = Console.ReadLine()!.ToLower();
 
-							continue;
+					try
+					{
+						switch (key)
+						{
+							case "??":
+								ConsoleHelper.PrintHelp();
+								continue;
+							case "auto_e":
+								WatchOptions.AutoExport = !WatchOptions.AutoExport;
+								ConsoleHelper.PrintOption(nameof(WatchOptions.AutoExport), WatchOptions.AutoExport);
+								continue;
+							case "auto_o":
+								WatchOptions.AutoOverwrite = !WatchOptions.AutoOverwrite;
+								ConsoleHelper.PrintOption(nameof(WatchOptions.AutoOverwrite), WatchOptions.AutoOverwrite);
+								continue;
+							default:
+								if (!CommandHelper.RunCommand(key, options))
+									return;
+
+								continue;
+						}
+					}
+					catch (Exception ex)
+					{
+						ConsolePrinter.PrintFatalError(ex.Message);
 					}
 				}
-				catch (Exception ex)
-				{
-					ConsolePrinter.PrintFatalError(ex.Message);
-				}
+			}
+			catch (Exception ex)
+			{
+				ConsolePrinter.PrintFatalError(ex.Message);
+				Console.ReadKey();
 			}
 		}
 	}
